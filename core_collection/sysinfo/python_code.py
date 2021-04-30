bef7d3b9ccd3d09b0224200a1c246ce810c96180
Leo Gordon <lg4@krai.ai>
2021-04-30 16:43:34 +0100

    sysinfo entry

---
+++ core_collection/sysinfo/python_code.py
@@ -0,0 +1,7 @@
+"""This entry can find out some properties of the host system,
+                lazily computed via parameters.
+
+Usage examples :
+                axs byname sysinfo , get osname
+                axs byname sysinfo , get arch
+"""
