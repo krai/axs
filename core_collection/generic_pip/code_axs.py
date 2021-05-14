7e21ceb7922344e52fd7867444011b6090babfbc
Leo Gordon <lg4@krai.ai>
2021-05-14 19:54:05 +0100

    a new subsystem for installing and using pip packages

---
+++ core_collection/generic_pip/code_axs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+""" This entry knows how to manipulate Python's module path to jump the queue.
+"""
+
+import sys
+
+
+def use(__entry__):
+    """Make an entry that contains an installed pip package the preferred location to import the package from.
+
+Usage examples :
+                axs np: byname numpy_1.16.4_pip , use , get np , ask_package_location
+    """
+    package_install_path = __entry__.get_path( __entry__['rel_packages_dir'] )
+    sys.path.insert(0, package_install_path)
+    return package_install_path
+
+
+def ask_package_version(package_name):
+    """Ask the pip package that is currently the preferred one for its version.
+        This could work both with a use'd or the one importable by default.
+
+Usage examples :
+                axs np: byname numpy_1.16.4_pip , use , get np , ask_package_version
+                
+                axs byname pip , ask_package_version numpy
+    """
+    module = __import__(package_name)
+    return getattr(module, '__version__')
+
+
+def ask_package_location(package_name):
+    """Ask the pip package that is currently the preferred one for its file location.
+        This could work both with a use'd or the one importable by default.
+
+Usage examples :
+                axs np: byname numpy_1.16.4_pip , use , get np , ask_package_location
+
+                axs byname pip , ask_package_location numpy
+    """
+    module = __import__(package_name)
+    return getattr(module, '__file__')
---
+++ core_collection/pip/code_axs.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+""" This entry knows how to install pip packages into generated entries.
+"""
+
+import os
+import sys
+
+
+def install(package_name, package_version=None, __entry__=None):
+    """Install a pip package into a separate entry, so that it could be easily use'd.
+
+Usage examples :
+                axs byname pip , install numpy 1.16.4
+
+                axs byname pip , install numpy
+    """
+    assert __entry__ != None, "__entry__ should be defined"
+    ak = __entry__.get_kernel()
+    assert ak != None, "__entry__'s kernel should be defined"
+
+    pip_entry_name = '_'.join( [package_name, package_version, 'pip'] ) if package_version else '_'.join( [package_name, 'pip'] )
+    work_collection = ak.work_collection()
+    pip_entry = work_collection.call('new', pip_entry_name)
+
+    rel_install_dir         = 'install'
+    extra_python_site_dir   = pip_entry.get_path( rel_install_dir )
+    os.makedirs( os.path.join(extra_python_site_dir, 'lib') )
+    os.symlink( 'lib', os.path.join(extra_python_site_dir, 'lib64') )
+
+    version_suffix                  = '=='+package_version if package_version!=None else ''
+
+    __entry__.call('run',  f"python3 -m pip install {package_name}{version_suffix} --prefix={extra_python_site_dir} --ignore-installed" )
+
+    pip_entry['rel_packages_dir']   = os.path.join( rel_install_dir, 'lib', f"python{sys.version_info.major}.{sys.version_info.minor}", 'site-packages' )
+    pip_entry['parent_entries^']    = [ '^byname:generic_pip' ]
+    pip_entry['package_name']       = package_name
+    pip_entry['package_version']    = package_version if package_version!=None else 'UNKNOWN'
+    pip_entry.save()
+
+    return pip_entry
