9e0b15b81bce949c857e3837b2edf9b3fadea049
Leo Gordon <lg4@krai.ai>
2021-06-01 14:53:33 +0100

    added pip_options

--- core_collection/pip/code_axs.py
+++ core_collection/pip/code_axs.py
@@ -7,13 +7,15 @@ import os
 import sys
 
 
-def install(package_name, package_version=None, __entry__=None):
+def install(package_name, package_version=None, pip_options=None, __entry__=None):
     """Install a pip package into a separate entry, so that it could be easily use'd.
 
 Usage examples :
                 axs byname pip , install numpy 1.16.4
 
                 axs byname pip , install numpy
+
+                axs byname pip , install scipy 1.5.1 --pip_options,=no-deps
     """
     assert __entry__ != None, "__entry__ should be defined"
     ak = __entry__.get_kernel()
@@ -28,9 +30,18 @@ Usage examples :
     os.makedirs( os.path.join(extra_python_site_dir, 'lib') )
     os.symlink( 'lib', os.path.join(extra_python_site_dir, 'lib64') )
 
-    version_suffix                  = '=='+package_version if package_version!=None else ''
+    version_suffix  = '=='+package_version if package_version!=None else ''
+    if pip_options:
+        if type(pip_options)==dict:
+            pip_options = [ k+'='+pip_options[k] for k in pip_options ]
 
-    __entry__.call('run',  f"python3 -m pip install {package_name}{version_suffix} --prefix={extra_python_site_dir} --ignore-installed" )
+        if type(pip_options)==list:
+            pip_options = ' '.join( [ e if e.startswith('-') else '--'+e for e in pip_options ] )
+
+    else:
+        pip_options=''
+
+    __entry__.call('run',  f"python3 -m pip install {package_name}{version_suffix} --prefix={extra_python_site_dir} --ignore-installed {pip_options}" )
 
     pip_entry['rel_packages_dir']       = os.path.join( rel_install_dir, 'lib', f"python{sys.version_info.major}.{sys.version_info.minor}", 'site-packages' )
     pip_entry['_parent_entries']        = [ [ "^", "byname", "generic_pip" ] ]
