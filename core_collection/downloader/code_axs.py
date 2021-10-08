03b8accd9769722f6521eb98f4ee3c34b2b6a97a
Leo Gordon <lg4@krai.ai>
2021-10-08 18:39:40 +0100

    allow download() and extract() result entries to not be explicitly given a name

--- core_collection/downloader/code_axs.py
+++ core_collection/downloader/code_axs.py
@@ -19,48 +19,41 @@ Cleaning up:
 
 """
 
-def download(url, entry_name, file_name, tags=None, __entry__=None):
+def download(url, file_name, tool_entry, tags=None, entry_name=None, __entry__=None):
     """Create a new entry and download the url into it
 
 Usage examples:
     # Manual downloading into a new entry:
-            axs byname downloader , download 'https://example.com' examplepage_downloaded example.html
+            axs byname downloader , download 'https://example.com/' example.html --entry_name=examplepage_downloaded
     # Resulting entry path (counter-intuitively) :
-            axs byname examplepage_downloaded , get_path ''
+            axs byquery downloaded,file_name=example.html , get_path ''
     # Downloaded file path:
-            axs byname examplepage_downloaded , get_path
+            axs byquery downloaded,file_name=example.html , get_path
     # Reaching to the original tool used for downloading:
-            axs byname examplepage_downloaded , get tool_path
+            axs byquery downloaded,file_name=example.html , get tool_entry , get tool_path
     # Clean up:
-            axs byname examplepage_downloaded , remove
+            axs byquery downloaded,file_name=example.html , remove
     """
 
     assert __entry__ != None, "__entry__ should be defined"
 
-    dep_name        = 'url_download_tool'
-    tool_entry      = __entry__[dep_name]
     tool_path       = tool_entry['tool_path']
 
-    if tool_path:
+    result_data = {
+        "url":          url,
+        "file_name":    file_name,
+        "tool_entry":   tool_entry,
+        "tags":         tags or [ "downloaded" ],
+    }
 
-        result_data = {
-            "url":          url,
-            "file_name":    file_name,
-            "tool_path":    tool_path,
-            "tags":         tags or [ "downloaded" ],
-        }
+    result_entry    = __entry__.get_kernel().work_collection().call('attached_entry', deterministic=False).own_data( result_data ).save( entry_name )
+    target_path     = result_entry.get_path(file_name)
 
-        result_entry    = __entry__.get_kernel().work_collection().call('attached_entry', deterministic=False).own_data( result_data ).save( entry_name )
-        target_path     = result_entry.get_path(file_name)
-
-        print(f"Dependency '{dep_name}' resolved into entry '{tool_entry.get_path()}' with the tool '{tool_path}', it will be used for downloading")
-        retval = tool_entry.call('run', [], {"url": url, "target_path": target_path})
-        if retval == 0:
-            return target_path
-        else:
-            print(f"Some problem occured when trying to download '{url}' into '{target_path}', bailing out")
-            result_entry.remove()
-            return None
+    print(f"The resolved tool_entry '{tool_entry.get_name()}' located at '{tool_entry.get_path()}' uses the shell tool '{tool_path}', which will be used for downloading")
+    retval = tool_entry.call('run', [], {"url": url, "target_path": target_path})
+    if retval == 0:
+        return target_path
     else:
-        print(f"Could not find a suitable downloader, so failed to download {url}")
+        print(f"A problem occured when trying to download '{url}' into '{target_path}', bailing out")
+        result_entry.remove()
         return None
--- core_collection/extractor/code_axs.py
+++ core_collection/extractor/code_axs.py
@@ -5,12 +5,12 @@
 
 import os
 
-def extract(archive_path, entry_name, tags=None, __entry__=None):
+def extract(archive_path, tool_entry, tags=None, entry_name=None, __entry__=None):
     """Create a new entry and extract the archive into it
 
 Usage examples:
     # Downloading the archive tarball:
-            axs byname downloader , download 'http://cKnowledge.org/ai/data/ILSVRC2012_img_val_500.tar' tarball_imagenet ILSVRC2012_img_val_500.tar
+            axs byname downloader , download 'http://cKnowledge.org/ai/data/ILSVRC2012_img_val_500.tar' ILSVRC2012_img_val_500.tar
     # Extracting the archive from one entry into another entry:
             axs byquery downloaded,file_name=ILSVRC2012_img_val_500.tar , archive_path: get_path , byname extractor , extract --entry_name=extracted_imagenet
     # Resulting entry path (counter-intuitively) :
@@ -19,38 +19,31 @@ Usage examples:
             axs byname extracted_imagenet , get_path
     # Clean up:
             axs byname extracted_imagenet , remove
-            axs byname tarball_imagenet , remove
+            axs byquery downloaded,file_name=ILSVRC2012_img_val_500.tar --- , remove
     """
 
     assert __entry__ != None, "__entry__ should be defined"
 
-    dep_name        = 'extraction_tool'
-    tool_entry      = __entry__[dep_name]
     tool_path       = tool_entry['tool_path']
     file_name       = 'extracted'
 
-    if tool_path:
+    result_data = {
+        "archive_path": archive_path,
+        "file_name":    file_name,
+        "tool_entry":   tool_entry,
+        "tags":         tags or [ "extracted" ],
+    }
 
-        result_data = {
-            "archive_path": archive_path,
-            "file_name":    file_name,
-            "tool_path":    tool_path,
-            "tags":         tags or [ "extracted" ],
-        }
+    result_entry    = __entry__.get_kernel().work_collection().call('attached_entry', deterministic=False).own_data( result_data ).save( entry_name )
+    target_path     = result_entry.get_path(file_name)
 
-        result_entry    = __entry__.get_kernel().work_collection().call('attached_entry', deterministic=False).own_data( result_data ).save( entry_name )
-        target_path     = result_entry.get_path(file_name)
+    os.makedirs( target_path )
 
-        os.makedirs( target_path )
-
-        print(f"Dependency '{dep_name}' resolved into entry '{tool_entry.get_path()}' with the tool '{tool_path}', it will be used for extracting")
-        retval = tool_entry.call('run', [], {"tool_path": tool_path, "archive_path": archive_path, "target_path": target_path})
-        if retval == 0:
-            return target_path
-        else:
-            print(f"Some problem occured when trying to extract '{archive_path}' into '{target_path}', bailing out")
-            result_entry.remove()
-            return None
+    print(f"The resolved tool_entry '{tool_entry.get_name()}' located at '{tool_entry.get_path()}' uses the shell tool '{tool_path}', which will be used for extracting")
+    retval = tool_entry.call('run', [], {"archive_path": archive_path, "target_path": target_path})
+    if retval == 0:
+        return target_path
     else:
-        print(f"Could not find a suitable extractor, so failed to extract {archive_path}")
+        print(f"A problem occured when trying to extract '{archive_path}' into '{target_path}', bailing out")
+        result_entry.remove()
         return None
