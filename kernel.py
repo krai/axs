b610553f874f6a50fbb50180985b2325000f5771
Leo Gordon <lg4@krai.ai>
2021-11-04 16:47:20 +0000

    created a separate imagenet500_recipe entry and integrated it into the torch_script_test workflow

--- kernel.py
+++ kernel.py
@@ -10,7 +10,7 @@ else:
     from kernel import default as ak
 """
 
-__version__ = '0.2.125'     # TODO: update with every kernel change
+__version__ = '0.2.126'     # TODO: update with every kernel change
 
 import logging
 import os
