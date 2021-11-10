628d7e27d719d931a711ed347e7446dc49de782a
Leo Gordon <lg4@krai.ai>
2021-11-10 16:24:30 +0000

    carved load_json() function to be able to link/include the data from script_output.json for seamless access via dig()

--- stored_entry.py
+++ stored_entry.py
@@ -183,6 +183,13 @@ Usage examples :
         return self
 
 
+    def load_json(self, json_file_path):
+        """Load a data structure from given JSON file.
+        """
+        with open( json_file_path ) as json_fd:
+            return json.load(json_fd)
+
+
     def own_data(self, data_dict=None):
         """Lazy-load, cache and return own data from the file system
 
@@ -199,9 +206,8 @@ Usage examples :
         elif self.own_data_cache is None:   # lazy-loading condition
             parameters_path = self.get_parameters_path()
             if os.path.isfile( parameters_path ):
-                with open( parameters_path ) as json_fd:
-                    self.own_data_cache = json.load(json_fd)
-                    self.touch('_AFTER_DATA_LOADING')
+                self.own_data_cache = self.load_json( parameters_path )
+                self.touch('_AFTER_DATA_LOADING')
             else:
                 logging.warning(f"[{self.get_name()}] parameters file {parameters_path} did not exist, initializing to empty parameters")
                 self.own_data_cache = {}
