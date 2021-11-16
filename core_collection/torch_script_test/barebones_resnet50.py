45b57ffe0c6261f33038891e59732c62be32d007
Leo Gordon <lg4@krai.ai>
2021-11-16 13:08:24 +0000

    added support for batching

--- core_collection/torch_script_test/barebones_resnet50.py
+++ core_collection/torch_script_test/barebones_resnet50.py
@@ -25,6 +25,7 @@ num_of_images       = int(sys.argv[2])
 model_name          = sys.argv[3]
 output_file_path    = sys.argv[4]       # if empty, recording of the output will be skipped
 execution_device    = sys.argv[5]       # if empty, it will be autodetected
+max_batch_size      = int(sys.argv[6])
 file_pattern        = 'ILSVRC2012_val_000{:05d}.JPEG'
 
 # sample execution (requires torchvision)
@@ -37,13 +38,26 @@ from torchvision import transforms
 execution_device    = execution_device or ('cuda' if torch.cuda.is_available() else 'cpu')  # autodetection
 torchvision_version = ':v' + torchvision.__version__.split('+')[0]
 
+
+def load_one_batch(indices):
+    file_names  = []
+    pre_batch   = []
+    for i in indices:
+        file_name = file_pattern.format(i+1)
+        file_path   = os.path.join( imagenet_dir, file_name )
+        input_image = Image.open( file_path ).convert('RGB')
+        input_tensor = preprocess(input_image)
+        file_names.append( file_name )
+        pre_batch.append(input_tensor)
+
+    return file_names, pre_batch
+
+
 ts_before_model_loading = time()
 
 model = torch.hub.load('pytorch/vision' + torchvision_version, model_name, pretrained=True)
 model.eval()
 model.to( execution_device )
-
-ts_before_data_loading  = time()
 
 preprocess = transforms.Compose([
     transforms.Resize(256),
@@ -52,46 +66,68 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-file_names  = []
-pre_batch   = []
-for i in range(num_of_images):
-    file_name = file_pattern.format(i+1)
-    file_path   = os.path.join( imagenet_dir, file_name )
-    input_image = Image.open( file_path ).convert('RGB')
-    input_tensor = preprocess(input_image)
-    file_names.append( file_name )
-    pre_batch.append(input_tensor)
+model_loading_s = time() - ts_before_model_loading
 
-input_batch = torch.stack(pre_batch, dim=0)
-input_batch = input_batch.to( execution_device )
+predictions             = {}
+sum_loading_s           = 0
+sum_inference_s         = 0
+list_batch_loading_s    = []
+list_batch_inference_s  = []
+for batch_start in range(0, num_of_images, max_batch_size):
+    batch_open_end = min(batch_start+max_batch_size, num_of_images)
+    ts_before_data_loading  = time()
 
-ts_before_inference     = time()
+    batch_file_names, pre_batch = load_one_batch( range(batch_start, batch_open_end) )
 
-with torch.no_grad():
-    output = model(input_batch)
+    input_batch = torch.stack(pre_batch, dim=0)
+    input_batch = input_batch.to( execution_device )
 
-# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-#print(torch.nn.functional.softmax(output[0], dim=0))
+    ts_before_inference     = time()
 
-class_numbers = torch.argmax(output, dim=1).tolist()
+    with torch.no_grad():
+        output = model(input_batch)
 
-ts_before_reporting     = time()
+    # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+    #print(torch.nn.functional.softmax(output[0], dim=0))
+
+    class_numbers = torch.argmax(output, dim=1).tolist()
+
+    ts_after_inference      = time()
+
+    predictions.update( dict(zip(batch_file_names, class_numbers)) )
+
+    batch_loading_s     = ts_before_inference - ts_before_data_loading
+    batch_inference_s   = ts_after_inference - ts_before_inference
+
+    list_batch_loading_s.append ( batch_loading_s )
+    list_batch_inference_s.append( batch_inference_s )
+
+    sum_loading_s   += batch_loading_s
+    sum_inference_s += batch_inference_s
+
+    if output_file_path:
+        print(f"[{batch_start+1}..{batch_open_end}] ", end='')
+    else:
+        print(f"{batch_start+1}..{batch_open_end}: {class_numbers}")
 
 if output_file_path:
     output_dict = {
         "execution_device": execution_device,
-        "time": {
-            "model_loading_s":  ts_before_data_loading - ts_before_model_loading,
-            "data_loading_s":   ts_before_inference - ts_before_data_loading,
-            "all_inference_s":  ts_before_reporting - ts_before_inference,
-            "per_inference_s":  (ts_before_reporting - ts_before_inference) / num_of_images,
-            "fps":              num_of_images / (ts_before_reporting - ts_before_inference),
+        "max_batch_size":   max_batch_size,
+        "times": {
+            "model_loading_s":          model_loading_s,
+
+            "sum_loading_s":            sum_loading_s,
+            "sum_inference_s":          sum_inference_s,
+            "list_batch_loading_s":     list_batch_loading_s,
+            "list_batch_inference_s":   list_batch_inference_s,
+
+            "per_inference_s":          sum_inference_s / num_of_images,
+            "fps":                      num_of_images / sum_inference_s,
         },
-        "predictions": dict( zip(file_names, class_numbers) )
+        "predictions": predictions,
     }
     json_string = json.dumps( output_dict , indent=4)
     with open(output_file_path, "w") as json_fd:
         json_fd.write( json_string+"\n" )
     print(f'Predictions for {num_of_images} images written into "{output_file_path}"')
-else:
-    print(class_numbers)
