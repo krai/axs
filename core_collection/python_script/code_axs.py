653515f81e919f22b2d80078e0df338c42c233f5
Leo Gordon <lg4@krai.ai>
2021-11-16 14:08:22 +0000

    bugfix: hopefully passing SYSTEMROOT to the new shell solves crash on Windows

--- core_collection/python_script/code_axs.py
+++ core_collection/python_script/code_axs.py
@@ -7,7 +7,17 @@ Usage examples:
                 axs byname python_script , run --abs_script_path=./barebones_resnet50.py --python_deps/:=^:byquery:python_package,package_name=torchvision,package_version=0.8.1
 """
 
+import os
+
 def ext_use_python_deps(python_deps=None):
     """Build PYTHONPATH from a list of resolved Python dependencies.
     """
-    return { 'PYTHONPATH': ':'.join( [ dep['abs_packages_dir'] for dep in python_deps ] ) } if python_deps else {}
+    new_env    = {}
+
+    if 'SYSTEMROOT' in os.environ:
+        new_env['SYSTEMROOT']   = os.environ['SYSTEMROOT']
+
+    if python_deps:
+        new_env['PYTHONPATH']   = ':'.join( [ dep['abs_packages_dir'] for dep in python_deps ] )
+
+    return new_env
