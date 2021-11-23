0f213f4e2c1c2c30f4bdffd092539939ad07dba1
Leo Gordon <lg4@krai.ai>
2021-11-23 18:39:41 +0000

    rule matching demonstrator entry added

---
+++ core_collection/rule_matching_demonstrator/code_axs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" This code is to demonstrate how the components of the query end up being passed into the producing function.
+"""
+
+def entry_producer(alpha, beta, gamma, delta=400, epsilon=500, zeta=600, tags=None, __record_entry__=None):
+
+    print("Running entry_producer with alpha={alpha}, beta={beta}, gamma={gamma}, delta={delta}, epsilon={epsilon}, zeta={zeta}")
+
+    return __record_entry__.save()
