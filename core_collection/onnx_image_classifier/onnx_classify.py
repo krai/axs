d617dca4730979f58fb616305420dda2d3afcc07
Maria Mirkina <maria@krai.ai>
2022-04-07 16:19:01 +0100

    Change output in onnx_classify.py like in the pytorch_image_classifier

--- core_collection/onnx_image_classifier/onnx_classify.py
+++ core_collection/onnx_image_classifier/onnx_classify.py
@@ -6,6 +6,8 @@ import sys
 import numpy as np
 from PIL import Image
 import onnxruntime as rt
+from time import time
+import json
 
 
 model_path          = sys.argv[1]
@@ -14,6 +16,7 @@ num_of_images       = int(sys.argv[3])
 max_batch_size      = int(sys.argv[4])
 class_names_path    = sys.argv[5]
 cpu_threads         = int(sys.argv[6])
+output_file_path    = sys.argv[7]
 
 file_pattern        = 'ILSVRC2012_val_000{:05d}.JPEG'
 normalize_data_bool = False
@@ -26,6 +29,8 @@ if cpu_threads > 0:
     sess_options.enable_sequential_execution = False
     sess_options.session_thread_pool_size = cpu_threads
 sess = rt.InferenceSession(model_path, sess_options)
+
+ts_before_model_loading = time()
 
 input_layer_names   = [ x.name for x in sess.get_inputs() ]
 input_layer_name    = input_layer_names[0]
@@ -93,20 +98,68 @@ def load_class_names(class_names_path):
 
 class_names = load_class_names(class_names_path)
 
+model_loading_s = time() - ts_before_model_loading
+
+predictions             = {}
+sum_loading_s           = 0
+sum_inference_s         = 0
+list_batch_loading_s    = []
+list_batch_inference_s  = []
+
 for batch_start in range(0, num_of_images, max_batch_size):
     batch_open_end = min(batch_start+max_batch_size, num_of_images)
+
+    ts_before_data_loading  = time()
 
     batch_global_indices = range(batch_start, batch_open_end)
     batch_filenames     = [ file_pattern.format(g+1) for g in batch_global_indices ]
     batch_data          = load_a_batch( batch_filenames )
+
+    ts_before_inference = time()
+
     batch_predictions   = sess.run([output_layer_name], {input_layer_name: batch_data})[0]
+
+    ts_after_inference      = time()
+
+    class_numbers       = (np.argmax( batch_predictions, axis=1) - 1).tolist()
+    predictions.update (dict(zip(batch_filenames, class_numbers)) )
+
+    batch_loading_s     = ts_before_inference - ts_before_data_loading
+    batch_inference_s   = ts_after_inference - ts_before_inference
+    list_batch_loading_s.append ( batch_loading_s )
+    list_batch_inference_s.append( batch_inference_s )
+    sum_loading_s   += batch_loading_s
+    sum_inference_s += batch_inference_s
+    #print(class_numbers)
 
     for i in range(batch_open_end-batch_start):
         softmax_vector      = batch_predictions[i][-1000:]
         top5_indices        = list(reversed(softmax_vector.argsort()))[:5]
+        #predictions[ batch_filenames[i] ] = top5_indices[0]
 
         print(batch_filenames[i] + ' :')
         for class_idx in top5_indices:
             print(f"\t{softmax_vector[class_idx]}\t{class_idx}\t{class_names[class_idx]}")
 
     print("-"*64 + "\n")
+
+if output_file_path:
+        output_dict = {
+            #"execution_device": execution_device,
+            "max_batch_size":   max_batch_size,
+            "times": {
+                "model_loading_s":          model_loading_s,
+                "sum_loading_s":            sum_loading_s,
+                "sum_inference_s":          sum_inference_s,
+                "per_inference_s":          sum_inference_s / num_of_images,
+                "fps":                      num_of_images / sum_inference_s,
+
+                "list_batch_loading_s":     list_batch_loading_s,
+                "list_batch_inference_s":   list_batch_inference_s,
+            },
+            "predictions": predictions,
+        }
+        json_string = json.dumps( output_dict , indent=4)
+        with open(output_file_path, "w") as json_fd:
+            json_fd.write( json_string+"\n" )
+        print(f'Predictions for {num_of_images} images written into "{output_file_path}"')
