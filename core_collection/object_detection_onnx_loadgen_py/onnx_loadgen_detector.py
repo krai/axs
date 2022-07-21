93e04418bde2942a89966f9323531759c708b747
Leo Gordon <lg4@krai.ai>
2022-07-21 14:43:50 +0100

    Loadgen-wired object detection program for COCO based on ONNX and Loadgen/COCO postprocessor

---
+++ core_collection/object_detection_onnx_loadgen_py/onnx_loadgen_detector.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python3
+
+"""Loadgen-wired Object Detection program based on ONNX.
+
+Usage examples  :
+
+                    # a short accuracy run:
+                axs byquery loadgen_output,detected_coco,framework=onnx,loadgen_dataset_size=20 , get mAP
+"""
+
+import array
+import os
+import sys
+import time
+
+import numpy as np
+import onnxruntime as rt
+from coco_loader import CocoLoader
+
+import mlperf_loadgen as lg
+
+
+scenario_str                = sys.argv[1]
+mode_str                    = sys.argv[2]
+dataset_size                = int(sys.argv[3])
+buffer_size                 = int(sys.argv[4])
+multistreamness_str         = sys.argv[5]
+count_override_str          = sys.argv[6]
+config_filepath             = sys.argv[7]
+verbosity                   = int( sys.argv[8] )
+model_path                  = sys.argv[9]
+model_name                  = sys.argv[10]
+execution_device            = sys.argv[11]          # if empty, it will be autodetected
+batch_size                  = int( sys.argv[12])
+cpu_threads                 = int( sys.argv[13])
+preprocessed_coco_dir       = sys.argv[14]
+coco_labels_file_path       = sys.argv[15]
+
+
+## Model properties:
+#
+INPUT_LAYER_NAME    = "image"
+OUTPUT_LAYER_BBOXES = "bboxes"
+OUTPUT_LAYER_LABELS = "labels"
+OUTPUT_LAYER_SCORES = "scores"
+MODEL_MAX_PREDICTIONS = 100
+MODEL_IMAGE_HEIGHT      = 1200
+MODEL_IMAGE_WIDTH       = 1200
+MODEL_IMAGE_CHANNELS    = 3
+SKIPPED_CLASSES         = [12,26,29,30,45,66,68,69,71,83]
+
+
+# Program parameters
+SCORE_THRESHOLD         = 0
+
+
+## Image normalization:
+#
+data_layout             = "NCHW"
+normalize_symmetric     = False
+subtract_mean_bool      = True
+given_channel_means     = [0.485, 0.456, 0.406]
+given_channel_stds      = [0.229, 0.224, 0.225]
+
+MODEL_INPUT_DATA_TYPE       = 'float32'
+
+
+## Preprocessed input images' properties:
+#
+IMAGE_LIST_FILE_NAME    = "original_dimensions.txt"
+original_dims_file_path = os.path.join(preprocessed_coco_dir, IMAGE_LIST_FILE_NAME)
+loader_object           = CocoLoader(preprocessed_coco_dir, original_dims_file_path, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH, data_layout, normalize_symmetric, subtract_mean_bool, given_channel_means, given_channel_stds)
+
+
+def load_labels(labels_filepath):
+    my_labels = []
+    input_file = open(labels_filepath, 'r')
+    for l in input_file:
+        my_labels.append(l.strip())
+    return my_labels
+
+class_labels    = load_labels(coco_labels_file_path)
+num_classes     = len(class_labels)
+bg_class_offset = 1
+class_map       = None
+if (SKIPPED_CLASSES):
+    class_map = []
+    for i in range(num_classes + bg_class_offset):
+        if i not in SKIPPED_CLASSES:
+            class_map.append(i)
+
+preprocessed_image_buffer   = None
+preprocessed_image_map      = np.empty(dataset_size, dtype=np.int)   # this type should be able to hold indices in range 0:dataset_size
+
+
+sess_options = rt.SessionOptions()
+if cpu_threads > 0:
+    sess_options.enable_sequential_execution = False
+    sess_options.session_thread_pool_size = cpu_threads
+
+if execution_device == "cpu":
+    requested_provider = "CPUExecutionProvider"
+elif execution_device in ["gpu", "cuda"]:
+    requested_provider = "CUDAExecutionProvider"
+elif execution_device in ["tensorrt", "trt"]:
+    requested_provider = "TensorrtExecutionProvider"
+
+sess = rt.InferenceSession(model_path, sess_options, providers= [requested_provider] if execution_device else rt.get_available_providers())
+
+session_execution_provider=sess.get_providers()
+print("Session execution provider: ", sess.get_providers(), file=sys.stderr)
+
+if "CUDAExecutionProvider" in session_execution_provider or "TensorrtExecutionProvider" in session_execution_provider:
+    print("Device: GPU", file=sys.stderr)
+else:
+    print("Device: CPU", file=sys.stderr)
+
+input_layer_names   = [ x.name for x in sess.get_inputs() ]
+input_layer_name    = input_layer_names[0]
+output_layer_names  = [ x.name for x in sess.get_outputs() ]
+output_layer_name   = output_layer_names[0]
+model_input_shape   = sess.get_inputs()[0].shape
+model_output_shape  = sess.get_outputs()[0].shape
+height              = model_input_shape[2]
+width               = model_input_shape[3]
+
+
+def tick(letter, quantity=1):
+    if verbosity:
+        print(letter + (str(quantity) if quantity>1 else ''), end='')
+
+
+def load_query_samples(sample_indices):     # 0-based indices in our whole dataset
+    global preprocessed_image_buffer
+
+    if verbosity > 1:
+        print("load_query_samples({})".format(sample_indices))
+
+    len_sample_indices = len(sample_indices)
+
+    tick('B', len_sample_indices)
+
+    if preprocessed_image_buffer is None:     # only do this once, once we know the expected size of the buffer
+        preprocessed_image_buffer = np.empty((len_sample_indices, MODEL_IMAGE_CHANNELS, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_WIDTH), dtype=MODEL_INPUT_DATA_TYPE)
+
+    for buffer_index, sample_index in zip(range(len_sample_indices), sample_indices):
+        preprocessed_image_map[sample_index] = buffer_index
+        preprocessed_image_buffer[buffer_index] = np.array( loader_object.load_image_by_index_and_normalize(sample_index)[0] )
+
+        tick('l')
+
+    if verbosity:
+        print('')
+
+
+def unload_query_samples(sample_indices):
+    #print("unload_query_samples({})".format(sample_indices))
+    tick('U')
+
+    if verbosity:
+        print('')
+
+
+def issue_queries(query_samples):
+
+    if verbosity > 2:
+        printable_query = [(qs.index, qs.id) for qs in query_samples]
+        print("issue_queries( {} )".format(printable_query))
+    tick('Q', len(query_samples))
+
+    run_options = rt.RunOptions()
+
+    for j in range(0, len(query_samples), batch_size):
+        batch       = query_samples[j:j+batch_size]   # NB: the last one may be shorter than batch_size in length
+        batch_data  = preprocessed_image_buffer[preprocessed_image_map[ [qs.index for qs in batch] ]]
+
+        begin_time = time.time()
+
+        batch_results = sess.run([OUTPUT_LAYER_BBOXES, OUTPUT_LAYER_LABELS, OUTPUT_LAYER_SCORES], {INPUT_LAYER_NAME: batch_data}, run_options)
+
+#        with open('output_tensors_loadgen.txt', 'w') as f:
+#            np.set_printoptions(threshold=sys.maxsize)
+#            print(batch_results, file=f)
+
+        inference_time_s = time.time() - begin_time
+
+        actual_batch_size = len(batch)
+        if verbosity > 1:
+            print("[batch of {}] inference={:.2f} ms".format(actual_batch_size, inference_time_s*1000))
+
+        tick('p', actual_batch_size)
+        if verbosity > 2:
+            print("predicted_batch_results = {}".format(batch_results))
+
+        response = []
+        response_array_refs = []    # This is needed to guarantee that the individual buffers to which we keep extra-Pythonian references, do not get garbage-collected.
+
+        for index_in_batch, qs in enumerate(batch):
+            global_image_index      = qs.index
+            width_orig, height_orig = loader_object.original_w_h[global_image_index]
+
+            reformed_active_boxes_for_this_sample = []
+            for i in range(len(batch_results[2][index_in_batch])):
+                confidence_score = batch_results[2][index_in_batch][i]
+                if confidence_score > SCORE_THRESHOLD:
+                    class_number = int(batch_results[1][index_in_batch][i])
+                    if class_map:
+                        class_number = class_map[class_number]
+
+                    (x1, y1, x2, y2) = batch_results[0][index_in_batch][i]
+
+                    reformed_active_boxes_for_this_sample += [
+                        float(global_image_index), y1, x1, y2, x2, confidence_score, class_number ]
+
+            response_array = array.array("B", np.array(reformed_active_boxes_for_this_sample, np.float32).tobytes())
+            response_array_refs.append(response_array)
+            bi = response_array.buffer_info()
+            response.append(lg.QuerySampleResponse(qs.id, bi[0], bi[1]))
+        lg.QuerySamplesComplete(response)
+        #tick('R', len(response))
+    sys.stdout.flush()
+
+
+
+def flush_queries():
+    pass
+
+
+
+def process_latencies(latencies_ns):
+    latencies_ms = [ (ns * 1e-6) for ns in latencies_ns ]
+    print("LG called process_latencies({})".format(latencies_ms))
+
+    if scenario_str == 'Offline':
+        latencies_ms   = (np.asarray(latencies_ms) - np.asarray([0] + latencies_ms[:-1])).tolist()
+        print("Offline latencies transformed to absolute time: {}".format(latencies_ms))
+
+    latencies_size      = len(latencies_ms)
+    latencies_avg       = int(sum(latencies_ms)/latencies_size)
+    latencies_sorted    = sorted(latencies_ms)
+    latencies_p50       = int(latencies_size * 0.5);
+    latencies_p90       = int(latencies_size * 0.9);
+    latencies_p99       = int(latencies_size * 0.99);
+
+    print("--------------------------------------------------------------------")
+    print("|                LATENCIES (in milliseconds and fps)               |")
+    print("--------------------------------------------------------------------")
+    print("Number of samples run:       {:9d}".format(latencies_size))
+    print("Min latency:                 {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[0], 1e3/latencies_sorted[0]))
+    print("Median latency:              {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[latencies_p50], 1e3/latencies_sorted[latencies_p50]))
+    print("Average latency:             {:9.2f} ms   ({:.3f} fps)".format(latencies_avg, 1e3/latencies_avg))
+    print("90 percentile latency:       {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[latencies_p90], 1e3/latencies_sorted[latencies_p90]))
+    print("99 percentile latency:       {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[latencies_p99], 1e3/latencies_sorted[latencies_p99]))
+    print("Max latency:                 {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[-1], 1e3/latencies_sorted[-1]))
+    print("--------------------------------------------------------------------")
+
+
+
+def benchmark_using_loadgen():
+    "Perform the benchmark using python API for the LoadGen library"
+
+    scenario = {
+        'SingleStream':     lg.TestScenario.SingleStream,
+        'MultiStream':      lg.TestScenario.MultiStream,
+        'Server':           lg.TestScenario.Server,
+        'Offline':          lg.TestScenario.Offline,
+    }[scenario_str]
+
+    mode = {
+        'AccuracyOnly':     lg.TestMode.AccuracyOnly,
+        'PerformanceOnly':  lg.TestMode.PerformanceOnly,
+        'SubmissionRun':    lg.TestMode.SubmissionRun,
+    }[mode_str]
+
+    ts = lg.TestSettings()
+    if(config_filepath):
+        ts.FromConfig(config_filepath, model_name, scenario_str)
+    ts.scenario = scenario
+    ts.mode     = mode
+
+    if multistreamness_str:
+        ts.multi_stream_samples_per_query = int(multistreamness_str)
+
+    if count_override_str:
+        ts.min_query_count = int(count_override_str)
+        ts.max_query_count = int(count_override_str)
+
+    sut = lg.ConstructSUT(issue_queries, flush_queries, process_latencies)
+    qsl = lg.ConstructQSL(dataset_size, buffer_size, load_query_samples, unload_query_samples)
+
+
+    sut = lg.ConstructSUT(issue_queries, flush_queries, process_latencies)
+    qsl = lg.ConstructQSL(dataset_size, buffer_size, load_query_samples, unload_query_samples)
+
+    log_settings = lg.LogSettings()
+    log_settings.enable_trace = False
+    lg.StartTestWithLogSettings(sut, qsl, ts, log_settings)
+
+    lg.DestroyQSL(qsl)
+    lg.DestroySUT(sut)
+
+
+try:
+    benchmark_using_loadgen()
+except Exception as e:
+    print('{}'.format(e))
+
