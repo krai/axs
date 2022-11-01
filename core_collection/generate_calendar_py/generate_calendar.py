a66456a862e1011eaa306b6c27e5b20f277980f8
Maria Mirkina <maria@krai.ai>
2022-11-01 17:21:45 +0000

    Add generate_calendar example

---
+++ core_collection/generate_calendar_py/generate_calendar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import calendar
+import json
+
+input_json_filepath     = sys.argv[1]
+output_json_filepath    = sys.argv[2]
+with open(input_json_filepath, 'r') as openfile:
+    input_dict = json.load(openfile)
+
+cal = calendar.TextCalendar(input_dict["first_weekday"])
+output_month = cal.formatmonth(input_dict["year"], input_dict["month"])
+
+output_dict = {
+    "calendar": output_month,
+}
+with open(output_json_filepath, "w") as outfile:
+    json.dump(output_dict, outfile, indent=4)
