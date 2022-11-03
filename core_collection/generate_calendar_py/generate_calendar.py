2591151eca2b1340ae6a2be3ad5b066ce119b4fb
Maria Mirkina <maria@krai.ai>
2022-11-03 22:04:31 +0000

    Added support for empty( or not ) output_json_file_path

--- core_collection/generate_calendar_py/generate_calendar.py
+++ core_collection/generate_calendar_py/generate_calendar.py
@@ -6,14 +6,18 @@ import json
 
 input_json_filepath     = sys.argv[1]
 output_json_filepath    = sys.argv[2]
+
 with open(input_json_filepath, 'r') as openfile:
     input_dict = json.load(openfile)
 
 cal = calendar.TextCalendar(input_dict["first_weekday"])
 output_month = cal.formatmonth(input_dict["year"], input_dict["month"])
 
-output_dict = {
-    "calendar": output_month,
-}
-with open(output_json_filepath, "w") as outfile:
-    json.dump(output_dict, outfile, indent=4)
+if output_json_filepath:
+    output_dict = {
+        "calendar": output_month,
+    }
+    with open(output_json_filepath, "w") as outfile:
+        json.dump(output_dict, outfile, indent=4)
+else:
+    print(output_month)
