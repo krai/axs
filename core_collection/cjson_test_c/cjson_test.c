9706f77fe42388bcc1a0245629e3d079f5d618c3
Maria Mirkina <maria@krai.ai>
2022-11-04 22:12:19 +0000

    Added writing to json file

--- core_collection/cjson_test_c/cjson_test.c
+++ core_collection/cjson_test_c/cjson_test.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "cJSON.h"
+
+char *convert_to_uppercase(char *str) {
+    char *newstr = malloc(strlen(str));
+    strcpy(newstr, str);
+    newstr[0] = newstr[0] + 'A' - 'a';
+    return newstr;
+}
 
 char *load_json_file(char *filename) {
     char *buffer = 0;
@@ -24,6 +32,11 @@ char *load_json_file(char *filename) {
 int main(int argc, char **argv) {
 
     char *input_json_string = load_json_file( argv[1] );
+    char *output_json_file_path = argv[2];
+
+    char *output_json_string = NULL;
+
+    cJSON *cjson_object = NULL;
 
     const cJSON *city   = NULL;
     const cJSON *alpha  = NULL;
@@ -32,6 +45,7 @@ int main(int argc, char **argv) {
 
     int status = 0;
     cJSON *parsed_json = cJSON_Parse(input_json_string);
+
     if(parsed_json == NULL) {
         const char *error_ptr = cJSON_GetErrorPtr();
         if(error_ptr != NULL) {
@@ -42,6 +56,7 @@ int main(int argc, char **argv) {
     }
 
     city = cJSON_GetObjectItemCaseSensitive(parsed_json, "city");
+
     if(cJSON_IsString(city) && (city->valuestring != NULL)) {
         printf("City name: %s\n", city->valuestring);
     }
@@ -60,6 +75,36 @@ int main(int argc, char **argv) {
     if(cJSON_IsNumber(gamma)) {
         printf("gamma (double): %f\n", gamma->valuedouble);
     }
+
+    // add values to output json file
+
+    cjson_object = cJSON_CreateObject();
+    cJSON *c = parsed_json->child;
+    while (c != NULL) {
+        switch (c->type) {
+            case cJSON_String: {
+                                   cJSON_AddStringToObject(cjson_object,convert_to_uppercase(c->string), c->valuestring);
+                                   break;
+                               }
+            case cJSON_Number: {
+                                   cJSON_AddNumberToObject(cjson_object,convert_to_uppercase(c->string), c->valuedouble);
+                                   break;
+                               }
+        };
+        c = c->next;
+    }
+
+    output_json_string = cJSON_Print(cjson_object);
+
+    cJSON_Delete(cjson_object);
+
+    FILE *f = fopen(output_json_file_path, "w");
+    fwrite(output_json_string, sizeof(char), strlen(output_json_string), f );
+
+    free(output_json_string);
+
+    fclose(f);
+
 end:
     cJSON_Delete(parsed_json);
     free(input_json_string);
