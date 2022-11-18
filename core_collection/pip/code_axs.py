8793b06b6448dcad96fb81a4eae68fcd47768dc5
Leo Gordon <lg4@krai.ai>
2022-11-18 13:10:47 +0000

    refactored pip entry (phase 1)

--- core_collection/pip/code_axs.py
+++ core_collection/pip/code_axs.py
@@ -9,7 +9,10 @@ import sys
 import ufun
 
 
-def install(package_name, package_version=None, pip_options=None, installable=None, python_tool_entry=None, pip_entry_name=None, python_major_dot_minor=None, tags=None,  __record_entry__=None):
+# Please note: some of the parameters below, although not directly used by the method,
+#   must still be present in order to be automatically recorded via __record_entry__ mechanism.
+#
+def install(package_name, pip_options=None, installable=None, python_tool_entry=None, pip_entry_name=None, rel_install_dir=None, rel_packages_dir=None, python_major_dot_minor=None, tags=None,  __record_entry__=None):
     """Install a pip package into a separate entry, so that it could be easily use'd.
 
 Usage examples :
@@ -25,26 +28,16 @@ Usage examples :
                 axs byname pip , install mlperf_loadgen 1.1 --installable=$HOME/work_collection/mlperf_inference_git/loadgen/dist/mlperf_loadgen-1.1-cp36-cp36m-macosx_10_9_x86_64.whl
     """
 
-    rel_install_dir = 'install'
-    logging.warning(f"The resolved python_tool_entry '{python_tool_entry.get_name()}' located at '{python_tool_entry.get_path()}' uses the shell tool '{python_tool_entry['tool_path']}'")
-
-    if sys.platform.startswith('win'):
-        rel_packages_dir    = os.path.join( rel_install_dir, 'lib', 'site-packages' )
-    else:
-        rel_packages_dir    = os.path.join( rel_install_dir, 'lib', 'python'+python_major_dot_minor, 'site-packages' )
-
     __record_entry__.pluck("pip_entry_name")
 
     __record_entry__["tags"]                = tags or ["python_package"]
     __record_entry__["_parent_entries"]     = [ [ "^", "byname", "base_pip_package" ] ]
     __record_entry__.parent_objects         = None      # reload parents
-
-    __record_entry__["rel_packages_dir"]    = rel_packages_dir
     __record_entry__.save( pip_entry_name )
 
-    extra_python_site_dir   = __record_entry__.get_path( rel_install_dir )
-    os.makedirs( os.path.join(extra_python_site_dir, 'lib') )
-    os.symlink( 'lib', os.path.join(extra_python_site_dir, 'lib64') )
+    abs_install_dir   = __record_entry__["abs_install_dir"]
+    os.makedirs( os.path.join(abs_install_dir, 'lib') )
+    os.symlink( 'lib', os.path.join(abs_install_dir, 'lib64') )
 
     if pip_options:
         if type(pip_options)==dict:
@@ -57,17 +50,10 @@ Usage examples :
         pip_options=''
 
     python_tool_entry.call('run', [], {
-        "shell_cmd": [ "^^", "substitute", "#{tool_path}#"+f" -m pip install {installable} --prefix={extra_python_site_dir} --ignore-installed {pip_options}" ],
+        "shell_cmd": [ "^^", "substitute", "#{tool_path}#"+f" -m pip install {installable} --prefix={abs_install_dir} --ignore-installed {pip_options}" ],
         "capture_output": False,
         "errorize_output": True,
     } )
-
-    # Recovering package_version from metadata and adding it to the Entry:
-    __record_entry__.parent_objects = None
-    version_from_metadata = __record_entry__.call('get_metadata', [], {'header_name': 'Version'})[0]
-    __record_entry__["package_version"] = version_from_metadata
-    __record_entry__["python_version"]  = python_tool_entry["tool_version"]
-    __record_entry__.save( pip_entry_name )
 
     return __record_entry__
 
