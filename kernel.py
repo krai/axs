159271098ecf784c0bd9eaad94b451658dc35113
Leo Gordon <lg4@krai.ai>
2023-03-29 13:51:33 +0100

    fix: use desired_python_version-based filtering directly in rule-matching (NB: remove for_python36 from all repos that contain it!)

--- kernel.py
+++ kernel.py
@@ -10,7 +10,7 @@ else:
     from kernel import default as ak
 """
 
-__version__ = '0.2.223'     # TODO: update with every kernel change
+__version__ = '0.2.224'     # TODO: update with every kernel change
 
 import logging
 import os
