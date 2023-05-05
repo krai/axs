84119ae973ba613314b502bd3a267a1af5224189
Maria Mirkina <maria@krai.ai>
2023-05-05 13:52:35 +0100

    Fixed an issue with type of normalize_symmetric for different models

--- core_collection/tf_image_classifier/tf_classify.py
+++ core_collection/tf_image_classifier/tf_classify.py
@@ -36,8 +36,7 @@ preprocessed_imagenet_dir   = input_parameters["preprocessed_images_dir"]
 resolution                  = input_parameters["resolution"]
 input_layer_name            = input_parameters["input_layer_name"]
 output_layer_name           = input_parameters["output_layer_name"]
-
-normalize_symmetric         = eval(input_parameters["normalization"]["normalize_symmetric"])
+normalize_symmetric         = input_parameters["normalization"]["normalize_symmetric"]
 subtract_mean_bool          = input_parameters["normalization"]["subtract_mean_bool"]
 given_channel_means         = eval(input_parameters["normalization"]["given_channel_means"])
 given_channel_stds          = eval(input_parameters["normalization"]["given_channel_stds"])
@@ -46,6 +45,9 @@ data_layout                 = input_parameters["normalization"]["data_layout"]
 num_of_images               = input_parameters["num_of_images"]
 max_batch_size              = input_parameters["max_batch_size"]
 top_n_max                   = input_parameters["top_n_max"]
+
+if model_name == "resnet50":
+    normalize_symmetric = eval(normalize_symmetric)
 
 batch_count                 = math.ceil(num_of_images / max_batch_size)
 
