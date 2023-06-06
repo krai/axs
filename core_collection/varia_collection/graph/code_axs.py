8093f91a9ffb69dd9a90aa75bebffe5a15c5b134
elim <elim@krai.ai>
2023-06-06 16:10:22 +0100

    Add dfs func for target node.

--- core_collection/varia_collection/graph/code_axs.py
+++ core_collection/varia_collection/graph/code_axs.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 import logging
+import re
 import graphviz
 from kernel import default_kernel as ak
 
@@ -34,103 +35,51 @@ def draw_collection(collection_object, tone_colour, graph):
 
     graph.subgraph(cluster)
 
-import re
+def dfs(root, f, __entry__):
+    stack = []
+    visited = set()
 
-def draw(target, return_this_entry=None, __entry__=None):
-    # To view graph
-    # display $(axs byquery graph_output,target=bert_squad_onnxruntime_py , get_path)/image.png
-
-    # __record_entry__.attach(__entry__.get_kernel().byname("work_collection"))
-    # __record_entry__.plant('tags', ['graph_output'])
-    # __record_entry__.save()
-    dest_dir = return_this_entry.get_path()
-    cur_target = target
+    cur_target_entry = __entry__.get_kernel().byname(root)
+    if not cur_target_entry:
+        print("ERROR!")
+        return 
+    stack.append((cur_target_entry, 0))
     
-    while(cur_target):
-    
-        cur_target_entry = __entry__.get_kernel().byname(cur_target)
-        print(f"cur_target: {cur_target_entry}")
-        parents = cur_target_entry.get("_parent_entries")
-        # print(f"Parent(s): {parents}")
-        output_entry = cur_target_entry.get("output_file_name")
-        # print(f"Output: {output_entry}")
+    while stack:
+        cur_target_entry, level = stack.pop()
+        cur_target = cur_target_entry.get_name()
+        print(cur_target)
+        if cur_target in visited:
+            continue
 
-        f = graphviz.Digraph(format='png')
-        f.attr('node', shape='circle')
         f.node(cur_target)
+        visited.add(cur_target)
 
-        output = False
-        output_parents_data = ""
-        cur_target_data = cur_target_entry.own_data()
-        for key, val in cur_target_data.items():
-            if "_parent_entries" in str(val):
-                output = True
-                output_parents_data = val
-            elif "tags" in str(val):
-                output = True
-
-        stack = []
-
-        # Add parent objects to the stack
+        parents = cur_target_entry.get("_parent_entries")
         if parents:
             for parent in parents:
-                stack.append((parent, 0))  # (object, level)
+                if isinstance(parent, str):
+                    p = __entry__.get_kernel().byname(parent)
+                else:
+                    p = parent
+                if not p:
+                    continue
+                stack.append((p, 0))
+                f.node(p.get_name())
+                f.edge(p.get_name(), cur_target)
 
-        visited = set()  # Track visited parent objects
-        parent_entries = {0: []}  # Parent entries dictionary for each level
-        parents_check = []
-        while stack != []:
-            parent, level = stack.pop()
-            print(f"stack: {parent.get_name()}, level: {level}")
-            print(type(parent))
-            parent_entry_check = __entry__.get_kernel().byname(parent)
-            print(f"parent_entry_check: {parent_entry_check}")
-            if parent_entry_check:
-                parents_check = parent_entry_check.get("_parent_entries")
-                print(f"parent_entries: {parents_check}")
+    return f
 
-            if parents_check:
+def draw(target, return_this_entry=None, __entry__=None):
+    dest_dir = return_this_entry.get_path()
 
-                # Add parent objects to the graph
-                if parent and __entry__.get_kernel() and __entry__.get_kernel().byname(parent):
-                    parent_entries[level] = parent.get_kernel().byname(parent).get("_parent_entries")
-                    for grandparent in parent_entries[level]:
-                        f.node(grandparent.get_name())
-                        f.edge(grandparent.get_name(), parent.get_name())
-                        if grandparent not in visited:  # Check if grandparent has been visited
-                            stack.append((grandparent, level + 1))
-            else:
-                print(f"No parent entries for {parent.get_name()}")
-                f.node(parent.get_name())
-                f.edge(parent.get_name(), cur_target)
-            
-            
-            visited.add(parent)  # Mark parent as visited
-            print(f"Visited Array: {visited}")
+    f = graphviz.Digraph(format='png')
+    f.attr('node', shape='circle')
+    f = dfs(target, f, __entry__)
 
-        print("Stack after iteration:", stack)
-        print("Visited set:", visited)
-
-    
-
-        if output:
-            f.node("output")
-            f.edge(cur_target, "output")
-
-        if output_parents_data:
-            info = find_parent(output_parents_data)
-            output_parents = find_byname(info)
-            f.node(output_parents)
-            f.edge(output_parents, "output")
-            cur_target_entry = output_parents
-        else:
-            cur_target_entry = None
-    
-        break
-
-    f.render(filename=f"{dest_dir}/image", view=True)
-    
+    f.render(filename=f"{dest_dir}/image")
     return return_this_entry
+
 
 def find_parent(obj):
     # obj = ['^^', 'execute', [[['get', '__record_entry__'], ['attach', ['^', 'work_collection']], ['plant', ['^^', 'substitute', [["_parent_entries", ['AS^IS', 'AS^IS', ['^', 'byname', 'base_bert_experiment']], 'tags', ['program_output', 'bert_squad'], 'model_name', '#{model_name}#', 'framework', '#{framework}#', 'output_file_name', '#{output_file_name}#', 'desired_python_version', '#{desired_python_version}#']]]], ['save'], ['get_path_from', 'output_file_name']]]] 
