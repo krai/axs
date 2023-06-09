f76c61030108a1c9c2225e2b8e67156a73e33634
saheli@krai.ai <saheli@krai.ai>
2023-06-09 15:54:27 +0100

    Update graph with parents of parents and multiple parents of output node.

--- core_collection/varia_collection/graph/code_axs.py
+++ core_collection/varia_collection/graph/code_axs.py
@@ -35,6 +35,7 @@ def draw_collection(collection_object, tone_colour, graph):
 
     graph.subgraph(cluster)
 
+
 def dfs(root, f, __entry__):
     stack = []
     visited = set()
@@ -72,47 +73,70 @@ def dfs(root, f, __entry__):
 
 def draw(target, return_this_entry=None, __entry__=None):
     dest_dir = return_this_entry.get_path()
+    target_entry = __entry__.get_kernel().byname(target)
+    output = False
+    output_parents_data = ""
+    target_data = target_entry.own_data()
+
+    for key, val in target_data.items():
+        if "_parent_entries" in str(val):
+            output = True
+            output_parents_data = val
+        elif "tags" in str(val):
+            output = True
 
     f = graphviz.Digraph(format='png')
     f.attr('node', shape='circle')
     f = dfs(target, f, __entry__)
+    if output:
+        f.node("output")
+        f.edge(target, "output")
+
+    if output_parents_data:
+        info = find_parent(output_parents_data)
+        output_parents = find_byname(info)
+        print("output_parents", output_parents)
+        for output_parent in output_parents:
+            f = dfs(output_parent, f, __entry__)
+            f.edge(output_parent, "output")
+            target_entry = output_parent
+        else:
+            target_entry = None
+
+        
 
     f.render(filename=f"{dest_dir}/image")
     return return_this_entry
 
-
 def find_parent(obj):
     # obj = ['^^', 'execute', [[['get', '__record_entry__'], ['attach', ['^', 'work_collection']], ['plant', ['^^', 'substitute', [["_parent_entries", ['AS^IS', 'AS^IS', ['^', 'byname', 'base_bert_experiment']], 'tags', ['program_output', 'bert_squad'], 'model_name', '#{model_name}#', 'framework', '#{framework}#', 'output_file_name', '#{output_file_name}#', 'desired_python_version', '#{desired_python_version}#']]]], ['save'], ['get_path_from', 'output_file_name']]]] 
-    item = find_key(obj, "_parent_entries")
-    return item
+    items = find_key(obj, "_parent_entries")
+    return items
 
 def find_byname(obj=None):
-    obj = ['_parent_entries', ['AS^IS', 'AS^IS', ['^', 'byname', 'base_bert_experiment']], 'tags', ['program_output', 'bert_squad'], 'model_name', '#{model_name}#', 'framework', '#{framework}#', 'output_file_name', '#{output_file_name}#', 'desired_python_version', '#{desired_python_version}#']
-    item = find_key(obj, "byname")
-
-    return list(item)[2]
+    obj = ['_parent_entries', ['AS^IS', 'AS^IS', ['^', 'byname', 'base_bert_loadgen_experiment'], ["^", "byname", "base_qaic_experiment"]], 'tags', ['program_output', 'bert_squad'], 'model_name', '#{model_name}#', 'framework', '#{framework}#', 'output_file_name', '#{output_file_name}#', 'desired_python_version', '#{desired_python_version}#']
+    items = find_key(obj, "byname")
+    return [list(item)[2] for item in items]
 
 def find_key(obj, key):
+    matches = []
     if isinstance(obj, dict):
         for k, v in obj.items():
             if k == key:
                 return v
+            matches.extend(find_key(v, key))
             result = find_key(v, key)
             if result is not None:
                 return result
     elif isinstance(obj, list):
         if key == "_parent_entries" and re.search(r"^\[(?:'|\")_parent_entries(.*)", str(obj)):
-            return obj
-        
+            matches.append(obj)
         if key == "byname" and re.search(r"^\[(?:'|\")\^(?:'|\")(?:\s*),(?:\s*)(?:'|\")byname(.*)", str(obj)):
-            return obj
-        
-        
+            matches.append(obj)
         for item in obj:
-            result = find_key(item, key)
-            if result is not None:
-                return result
-    return None
+            matches.extend(find_key(item, key))
 
+    return matches
+        
 
 
