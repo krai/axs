e1c04f53860ac90174aa1c45e5a3a622cba2f527
Leo Gordon <lg4@krai.ai>
2024-03-06 16:43:14 +0000

    refactored downloader to inherit from entry_creator

--- core_collection/essentials_collection/downloader/code_axs.py
+++ core_collection/essentials_collection/downloader/code_axs.py
@@ -40,7 +40,7 @@ def get_uncompressed_split_file_path(split_file_path, uncompress_format):
         return split_file_path
 
 
-def download(url, file_name=None, split_file_path=None, md5=None, downloading_tool_entry=None, uncompress_format=None, tags=None, extra_tags=None, entry_name=None, __entry__=None, __record_entry__=None):
+def download(url, abs_result_path, stored_newborn_entry, newborn_entry_path, md5=None, uncompress_format=None, downloading_tool_entry=None, md5_tool_entry=None, uncompress_tool_entry=None):
     """Create a new entry and download the url into it
 
 Usage examples:
@@ -69,54 +69,46 @@ Usage examples:
             axs byname downloader , download --downloading_tool_query+=_from_google_drive --url=https://drive.google.com/uc?id=1XRfiA8wtZEo6SekkJppcnfEr4ghQAS4g --file_path=hello2.text
     """
 
-    __record_entry__.pluck( "entry_name" )
+    if downloading_tool_entry:
+        logging.warning(f"The resolved downloading_tool_entry '{downloading_tool_entry.get_name()}' located at '{downloading_tool_entry.get_path()}' uses the shell tool '{downloading_tool_entry['tool_path']}'")
+        retval = downloading_tool_entry.call('run', [], {"url": url, "target_path": abs_result_path, "record_entry_path": newborn_entry_path})
+        if retval != 0:
+            logging.error(f"A problem occured when trying to download '{url}' into '{abs_result_path}', bailing out")
+            stored_newborn_entry.remove()
+            return None
+    else:
+        logging.error(f"Downloading of {url} requested, but failed to detect a suitable tool")
+        stored_newborn_entry.remove()
+        return None
 
-    __record_entry__["tags"] = list(set( (tags or []) + extra_tags ))
-    __record_entry__.pluck( "extra_tags" )
-
-    __record_entry__.save( entry_name )
-
-    record_entry_path   = __record_entry__.get_path( "" )
-    target_path         = __record_entry__.get_path(split_file_path)
-
-
-    logging.warning(f"The resolved downloading_tool_entry '{downloading_tool_entry.get_name()}' located at '{downloading_tool_entry.get_path()}' uses the shell tool '{downloading_tool_entry['tool_path']}'")
-    retval = downloading_tool_entry.call('run', [], {"url": url, "target_path": target_path, "record_entry_path": record_entry_path})
-    if retval == 0:
-        if md5 is not None:
-            md5_tool_entry = __entry__['md5_tool_entry']
+    if md5:
+        if md5_tool_entry:
             logging.warning(f"The resolved md5_tool_entry '{md5_tool_entry.get_name()}' located at '{md5_tool_entry.get_path()}' uses the shell tool '{md5_tool_entry['tool_path']}'")
-            computed_md5 = md5_tool_entry.call('run', [], {"file_path": target_path})
+            computed_md5 = md5_tool_entry.call('run', [], {"file_path": abs_result_path})
             if computed_md5 != md5:
                 logging.error(f"The computed md5 sum '{computed_md5}' is different from the expected '{md5}', bailing out")
-                __record_entry__.remove()
+                stored_newborn_entry.remove()
                 return None
             else:
-                __record_entry__['md5_tool_entry'] = md5_tool_entry
                 logging.warning(f"The computed md5 sum '{computed_md5}' matched the expected one.")
-
-        if uncompress_format:
-            __entry__['uncompress_format'] = uncompress_format  # FIXME: this should not need to be explicitly mentioned, it should be automagically transferred over
-            uncompress_tool_entry = __entry__['uncompress_tool_entry']
-            __entry__['uncompress_format'] = ""                 # NB: avoid screwing up parallel invocations of the same function in context of its entry
-            if uncompress_tool_entry:
-                logging.warning(f"Uncompression from {uncompress_format} requested")
-                retval = uncompress_tool_entry.call('run', [], {"target_path": target_path, "in_dir": record_entry_path})
-                if retval == 0:
-                    __record_entry__['uncompress_tool_entry'] = uncompress_tool_entry
-                else:
-                    logging.error(f"could not uncompress {target_path}, bailing out")
-                    __record_entry__.remove()
-                    return None
-            else:
-                __record_entry__['uncompression'] = 'FAILED'
-                logging.error(f"Uncompression from {uncompress_format} requested, but failed to detect such tool")
         else:
-            logging.warning(f"Uncompression not requested")
+            logging.error(f"Checking md5 against the value {md5} requested, but failed to detect a suitable tool")
+            stored_newborn_entry.remove()
+            return None
 
-        __record_entry__.save()
-        return __record_entry__
-    else:
-        logging.error(f"A problem occured when trying to download '{url}' into '{target_path}', bailing out")
-        __record_entry__.remove()
-        return None
+    if uncompress_format:
+        if uncompress_tool_entry:
+            logging.warning(f"The resolved uncompress_tool_entry '{uncompress_tool_entry.get_name()}' located at '{uncompress_tool_entry.get_path()}' uses the shell tool '{uncompress_tool_entry['tool_path']}'")
+            retval = uncompress_tool_entry.call('run', [], {"target_path": abs_result_path, "in_dir": newborn_entry_path})
+            if retval != 0:
+                logging.error(f"A problem occured when trying to uncompress {abs_result_path}, bailing out")
+                stored_newborn_entry.remove()
+                return None
+            else:
+                logging.error(f"Uncompression from {uncompress_format} successful")
+        else:
+            logging.error(f"Uncompression from {uncompress_format} requested, but failed to detect a suitable tool")
+            stored_newborn_entry.remove()
+            return None
+
+    return stored_newborn_entry
