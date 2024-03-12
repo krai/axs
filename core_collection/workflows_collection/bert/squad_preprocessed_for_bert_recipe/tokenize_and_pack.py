691e798f5298ddbdb3def1e369aef6763601ef08
Leo Gordon <lg4@krai.ai>
2024-03-12 14:42:29 +0000

    less dancing around with the paths

--- core_collection/workflows_collection/bert/squad_preprocessed_for_bert_recipe/tokenize_and_pack.py
+++ core_collection/workflows_collection/bert/squad_preprocessed_for_bert_recipe/tokenize_and_pack.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import os
+import pickle
 import sys
 
+import numpy as np
 from transformers import BertTokenizer
 
 # Input and output file paths:
@@ -23,14 +25,14 @@ if calibration:
 
 from create_squad_data import read_squad_examples, convert_examples_to_features
 
-print("Creating the tokenizer from {} ...".format(tokenization_vocab_path))
+print( f"Creating the tokenizer from {tokenization_vocab_path} ..." )
 tokenizer = BertTokenizer(tokenization_vocab_path)
 
-print("Reading examples from {} ...".format(squad_original_path))
+print( f"Reading examples from {squad_original_path} ..." )
 examples = read_squad_examples(input_file=squad_original_path, is_training=False, version_2_with_negative=False)
 
 if calibration:
-    print("Calibrating dataset with examples in ", calibration_data_path)
+    print( f"Calibrating dataset with examples in {calibration_data_path}" )
     if calibration_option == "features":
         calib_examples = []
         with open(calibration_data_path, 'r') as fp:
@@ -49,7 +51,7 @@ if calibration:
                 try:
                     calib_examples.append(examples_ids_dict[qas_id])
                 except:
-                    print(f"qas_id {qas_id} from calibration file doesn't exist in original dataset")
+                    print( f"qas_id {qas_id} from calibration file doesn't exist in original dataset" )
                     continue
             examples = calib_examples
 
@@ -57,7 +59,7 @@ eval_features = []
 def append_feature(feature):
     eval_features.append(feature)
 
-print("Tokenizing examples to features (max_seq_length={}, max_query_length={}, doc_stride={}) ...".format(max_seq_length, max_query_length, doc_stride))
+print( f"Tokenizing examples to features (max_seq_length={max_seq_length}, max_query_length={max_query_length}, doc_stride={doc_stride}) ..." )
 convert_examples_to_features(
     examples=examples,
     tokenizer=tokenizer,
@@ -71,9 +73,9 @@ convert_examples_to_features(
 
 # raw
 
-import numpy as np
+print( f"Recording features to {tokenized_squad_dir}/*.raw ..." )
 
-print("Recording features to {} ...".format(tokenized_squad_dir + "*.raw"))
+deep_prefix = os.path.join(tokenized_squad_dir, tokenized_squad_files_prefix)
 
 num_features = len(eval_features)
 input_ids = np.zeros((num_features, max_seq_length), dtype=np.int64)
@@ -88,16 +90,15 @@ for idx, feature in enumerate(eval_features):
     input_mask[idx, :] = np.array(feature.input_mask, dtype=np.int64)
     segment_ids[idx, :] = np.array(feature.segment_ids, dtype=np.int64)
 
-input_ids.astype('int64').tofile(tokenized_squad_dir + tokenized_squad_files_prefix + "_input_ids.raw")
-input_mask.astype('int64').tofile(tokenized_squad_dir + tokenized_squad_files_prefix + "_input_mask.raw")
-segment_ids.astype('int64').tofile(tokenized_squad_dir + tokenized_squad_files_prefix + "_segment_ids.raw")
+input_ids.astype('int64').tofile( deep_prefix + "_input_ids.raw")
+input_mask.astype('int64').tofile( deep_prefix + "_input_mask.raw")
+segment_ids.astype('int64').tofile( deep_prefix + "_segment_ids.raw")
+
 
 # pickle
 
-import pickle
+pickled_filename =  deep_prefix + ".pickled"
     
-print("Recording features to {} ...".format(tokenized_squad_dir + tokenized_squad_files_prefix + ".pickled"))
-with open(tokenized_squad_dir + tokenized_squad_files_prefix + ".pickled", 'wb') as cache_file:
+print( f"Recording features to {pickled_filename} ..." )
+with open(pickled_filename, 'wb') as cache_file:
     pickle.dump(eval_features, cache_file)
-
-
