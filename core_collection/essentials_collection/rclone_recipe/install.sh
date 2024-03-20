e1e12403b0f076aa94e72da079a6724344ae39cc
Maria Mirkina <maria@krai.ai>
2024-03-20 13:12:14 -0500

    Added install.sh to rclone_recipe

---
+++ core_collection/essentials_collection/rclone_recipe/install.sh
@@ -0,0 +1,63 @@
+
+#detect the platform
+OS="$(uname)"
+case $OS in
+  Linux)
+    OS='linux'
+    ;;
+  FreeBSD)
+    OS='freebsd'
+    ;;
+  NetBSD)
+    OS='netbsd'
+    ;;
+  OpenBSD)
+    OS='openbsd'
+    ;;  
+  Darwin)
+    OS='osx'
+    binTgtDir=/usr/local/bin
+    man1TgtDir=/usr/local/share/man/man1
+    ;;
+  SunOS)
+    OS='solaris'
+    echo 'OS not supported'
+    exit 2
+    ;;
+  *)
+    echo 'OS not supported'
+    exit 2
+    ;;
+esac
+
+OS_type="$(uname -m)"
+case "$OS_type" in
+  x86_64|amd64)
+    OS_type='amd64'
+    ;;
+  i?86|x86)
+    OS_type='386'
+    ;;
+  aarch64|arm64)
+    OS_type='arm64'
+    ;;
+  armv7*)
+    OS_type='arm-v7'
+    ;;
+  armv6*)
+    OS_type='arm-v6'
+    ;;
+  arm*)
+    OS_type='arm'
+    ;;
+  *)
+    echo 'OS type not supported'
+    exit 2
+    ;;
+esac
+
+
+
+download_link="https://beta.rclone.org/rclone-beta-latest-${OS}-${OS_type}.zip"
+echo $download_link
+
