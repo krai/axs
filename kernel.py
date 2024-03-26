6f4887a931e5a0ad5730e1fdd9d3732795596267
Leo Gordon <lg4@krai.ai>
2024-03-26 15:06:36 +0000

    added newborn_entry_param_mapping{} - the most flexible interface to entry_creator

--- kernel.py
+++ kernel.py
@@ -10,7 +10,7 @@ else:
     from kernel import default as ak
 """
 
-__version__ = '0.2.322'     # TODO: update with every kernel change
+__version__ = '0.2.323'     # TODO: update with every kernel change
 
 import logging
 import os
