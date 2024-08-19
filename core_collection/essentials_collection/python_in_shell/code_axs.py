a4c2c9fbf7f2538fc15df14273da2df02a732e55
Leo Gordon <lg4@krai.ai>
2024-08-19 19:11:11 +0000

    bugfix: make sure PATH from python_deps comes before systems PATH, to improve reproducibility

--- core_collection/essentials_collection/python_in_shell/code_axs.py
+++ core_collection/essentials_collection/python_in_shell/code_axs.py
@@ -24,10 +24,14 @@ def ext_use_python_deps(python_deps=None, inherit_env_keys=None, extra_env=None)
     if python_deps:     # Note the OS-independent way of joining individual paths
         new_env['PYTHONPATH']   = os.pathsep.join( [ dep if type(dep)==str else ( dep.get('abs_packages_dir') or dep.get_path(dep.get('file_name')) ) for dep in python_deps ] )
 
-        path_parts  = [ new_env.get('PATH') ] if new_env.get('PATH') else []
+        path_parts  = []
         for dep in python_deps:
             if dep and type(dep)!=str and dep.get('abs_bin_dir'):
                 path_parts.append( dep.get('abs_bin_dir') )
+
+        if new_env.get('PATH'):
+            path_parts.append( new_env.get('PATH') )
+
         new_env['PATH'] = os.pathsep.join( path_parts )
 
     return new_env
