0de9baf7069dc863b27d994e45f9cdcb064b4229
Leo Gordon <lg4@krai.ai>
2024-10-15 13:16:45 +0100

    support for docker_image_tag

--- kernel.py
+++ kernel.py
@@ -10,7 +10,7 @@ else:
     from kernel import default as ak
 """
 
-__version__ = '0.2.392'     # TODO: update with every kernel change
+__version__ = '0.2.393'     # TODO: update with every kernel change
 
 import logging
 import os
