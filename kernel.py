7d69b2a08cb1bebec4e5bb5b9c2f1532ea5083a9
Leo Gordon <lg4@krai.ai>
2024-10-23 15:09:06 +0100

    bugfix: modify the query to avoid infinite recursion

--- kernel.py
+++ kernel.py
@@ -10,7 +10,7 @@ else:
     from kernel import default as ak
 """
 
-__version__ = '0.2.404'     # TODO: update with every kernel change
+__version__ = '0.2.405'     # TODO: update with every kernel change
 
 import logging
 import os
