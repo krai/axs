8a7d501c6f1e6e89d51d53717d89216977221abd
Leo Gordon <lg4@krai.ai>
2024-11-07 17:10:12 +0000

    example of generalized mounting of host entries into the container and their pick-up from within

--- core_collection/remotes_collection/entries_from_docker/code_axs.py
+++ core_collection/remotes_collection/entries_from_docker/code_axs.py
@@ -8,3 +8,12 @@ Build the image (make sure you don't have an outdated axs kernel in docker's bui
 To run a pipeline in that docker image, run the following on the host:
     axs byquery program_output,remote_type=docker,exchange_type=entries,task=image_classification,framework=onnxrt
 """
+
+def host_entries_mapping(host_entries_to_pass_down):
+
+    return ' '.join( [ f"-v {he.get_path()}:/mnt/{he.get_name()}" for he in host_entries_to_pass_down ] )
+
+
+def container_entries_access(host_entries_to_pass_down):
+
+    return 'axs work_collection ' + ( ' '.join( [ f", add_entry_path /mnt/{he.get_name()}" for he in host_entries_to_pass_down ] ) )
