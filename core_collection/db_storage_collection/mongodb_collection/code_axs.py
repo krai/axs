b04d278e80087e2330d292804f44f649296a1752
Leo Gordon <lg4@krai.ai>
2025-07-16 12:21:51 +0100

    docfix: documented how to use database-based collections

--- core_collection/db_storage_collection/mongodb_collection/code_axs.py
+++ core_collection/db_storage_collection/mongodb_collection/code_axs.py
@@ -1,16 +1,33 @@
 """
     Usage examples :
-                axs byname mongodb_collection , all_byquery 'age<99' --template='Name: #{name}# , Age: #{age}#'
 
-                axs byname mongodb_collection , byquery name=Leo , own_data
+                axs byname mongodb_collection , help                                                                                                                        # see this cookbook
 
-                axs byname mongodb_collection , byquery name=Leo , remove
+        Working with an existing database-based collection:
 
-                axs byname mongodb_collection , attached_entry ---own_data='{"_parent_entries":[["^","byname","mongodb_entry"]],"name":"Leo","age":48}' , save
+                axs byname people_collection , all_byquery 'age<99' --template='Name: #{name}# , Age: #{age}#'                                                              # list entries via a report
 
-                axs byname mongodb_collection , byquery name=Leo , plant age 49 , save
+                axs byname people_collection , byquery name=Leo , own_data                                                                                                  # find one entry
 
-                axs byname mongodb_collection , byquery name=Leo , own_data
+                axs byname people_collection , byquery name=Leo , remove                                                                                                    # remove it
+
+                axs byname people_collection , attached_entry ---own_data='{"_parent_entries":[["^","byname","mongodb_entry"]],"name":"Leo","age":48}' , save               # reinsert it
+
+                axs byname people_collection , byquery name=Leo , plant age 49 , save                                                                                       # modify it
+
+                axs byname people_collection , byquery name=Leo , own_data                                                                                                  # check the result
+
+
+        Working with a new database-based collection:
+
+                axs work_collection , attached_entry ---own_data='{"_parent_entries":[["^","byname","mongodb_collection"]],"tags":["collection"]}' , save foo_collection    # make a new database-based collection
+
+                axs byname foo_collection , attached_entry ---own_data='{"_parent_entries":[["^","byname","mongodb_entry"]],"name":"John","age":98}' , save                 # store an entry into it
+
+                axs byname foo_collection , all_byquery ''                                                                                                                  # list all entries in it (includes the collection)
+
+                axs byname foo_collection , all_byquery 'age<100'                                                                                                           # list meaningful entries (excludes the collection)
+
 """
 
 
