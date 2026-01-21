330a4fc13ae3d55c97df53baa65ef3c946d95540
Leo Gordon <lg4@krai.ai>
2026-01-21 16:05:25 +0000

    a package for installing "gh" from a precompiled binary

---
+++ core_collection/essentials_collection/gh_recipe/code_axs.py
@@ -0,0 +1,13 @@
+"""
+    A package for installing a precompiled "gh" binary into axs environment.
+
+    Usage examples :
+
+        # get version:
+            axs byquery extracted,gh , run --shell_cmd_with_subs='#{gh_path}# --version'
+
+        # get help:
+            axs byquery extracted,gh , run --shell_cmd_with_subs='#{gh_path}# --help'
+
+        # any other command - just roll it yourself from the template above
+"""
