1da9c4d30f6a020b487eb4554dabc9dff60f2acd
Leo Gordon <lg4@krai.ai>
2026-03-06 12:09:12 +0000

    allow creating collections within collection + documented non-intuitive syntax

--- core_collection/essentials_collection/make_collection/code_axs.py
+++ core_collection/essentials_collection/make_collection/code_axs.py
@@ -15,5 +15,10 @@
         A collection may be set up auto-indexing, i.e. not updating the index every time a contained entry is added or removed - good for concurrent execution of multiple axs's
 
                 axs byquery collection,collection_name=vehicle_collection,contained_entries=auto --parent_recursion+    # auto-indexing new collection
+
+
+        Adding a collection into another collection: containing_collection_name is a "second class" parameter not expected to end up in the created entry
+
+                axs byquery collection,collection_name=ev_collection --,::=containing_collection_name:vehicle_collection --parent_recursion+    # make ev_collection inside vehicle_collection
 """
 
