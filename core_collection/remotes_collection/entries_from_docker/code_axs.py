2e338fa6859b18b8b9244bab73551c23df32ae3a
Leo Gordon <lg4@krai.ai>
2026-05-11 19:07:35 +0100

    auto-mounting axs kernel path inside the container to be able to use it without installation (and ensure kernel version sync)

--- core_collection/remotes_collection/entries_from_docker/code_axs.py
+++ core_collection/remotes_collection/entries_from_docker/code_axs.py
@@ -9,11 +9,11 @@ To run a pipeline in that docker image, run the following on the host:
     axs byquery program_output,remote_type=docker,exchange_type=entries,task=image_classification,framework=onnxrt
 """
 
-def host_entries_mapping(host_entries_to_pass_down, host_paths_to_pass_down):
+def host_entries_mapping(host_entries_to_pass_down, host_paths_to_pass_down, host_kernel_path):
 
-    return ' '.join( [ f"-v {he.get_path()}:/mnt/{he.get_name()}" for he in host_entries_to_pass_down ] + [ f"-v {hp}:{hp}" for hp in host_paths_to_pass_down ] )
+    return ' '.join( [ f"-v {host_kernel_path}:/mnt/axs" ] + [ f"-v {he.get_path()}:/mnt/{he.get_name()}" for he in host_entries_to_pass_down ] + [ f"-v {hp}:{hp}" for hp in host_paths_to_pass_down ] )
 
 
-def container_entries_access(host_entries_to_pass_down):
+def container_entries_access(host_entries_to_pass_down, in_docker_kernel_path):
 
-    return 'axs work_collection ' + ( ' '.join( [ f", add_entry_path /mnt/{he.get_name()}" for he in host_entries_to_pass_down ] ) )
+    return f'{in_docker_kernel_path}/axs work_collection ' + ( ' '.join( [ f", add_entry_path /mnt/{he.get_name()}" for he in host_entries_to_pass_down ] ) )
